86f2d7fb93c35bf2ed7592141a1fdb1f93d56dbe
a22nicolasmr <a22nicolasmr@iessanclemente.net>
2025-01-21 14:29:34 +0100

    sexo honestamente

---
+++ 2AVALIACION/WP/app/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo "¡Hola, Mundo!"; ?></h1>
+
+    <!-- sudo docker run -it --rm --name certbot \
+  -v "/etc/letsencrypt:/etc/letsencrypt" \
+  -v "/var/lib/letsencrypt:/var/lib/letsencrypt" \
+  -p 80:80 \
+  certbot/certbot certonly -->
+
+</body>
+
+</html>
